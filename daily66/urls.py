ab8b185914780c188103236e9c346f2a82d96cf1
Hyejeong Park <phj1698@gmail.com>
2016-04-13 00:39:51 +0900

    Add view

--- daily66/urls.py
+++ daily66/urls.py
@@ -20,4 +20,5 @@ import main.views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', main.views.index),
+    url(r'^@(?P<username>[\w-]+)/$', main.views.habit_list),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Habit
 
 def index(request):
+    return render(request, 'main/index.html')
+
+def habit_list(request, username):
     return render(request, 'main/index.html')
