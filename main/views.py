3cb1809c0e3ca4f74070baf2536adcb794e1770a
Hyejeong Park <phj1698@gmail.com>
2016-04-15 18:31:25 +0900

    Add user_list and habit_list #5

--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from .models import Habit
 
 def index(request):
-    return render(request, 'main/index.html')
+    user_list = User.objects.all()
+    habit_list = Habit.objects.all()
+    return render(request, 'main/index.html', {'user_list': user_list,'habit_list': habit_list,})
 
 def habit_list(request, username):
     return render(request, 'main/index.html')
