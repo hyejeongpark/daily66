d1c4bc26f94afc5514517e5fe00b0e6da6268a02
Hyejeong Park <phj1698@gmail.com>
2016-04-17 15:12:41 +0900

    Create Log model

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Habit
+from .models import Log
 
 admin.site.register(Habit)
+admin.site.register(Log)
--- main/models.py
+++ main/models.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import User
 class Habit(models.Model):
     user = models.ForeignKey(User)
     title = models.CharField(max_length=200)
+
+class Log(models.Model):
+    habit = models.ForeignKey(Habit)
+    date = models.DateField()
+    content = models.CharField(max_length=500)
+    score = models.IntegerField()
+
