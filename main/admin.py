76fce6241dd005b13d461a96edd4005420e1a417
Hyejeong Park <phj1698@gmail.com>
2016-04-19 16:22:48 +0900

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Habit
 from .models import Log
 
-admin.site.register(Habit)
-admin.site.register(Log)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user')
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('habit', 'date')
+
+admin.site.register(Habit, HabitAdmin)
+admin.site.register(Log, LogAdmin)
