b345e937f2333306fa9214dda7ab82f45577f65b
Hyejeong Park <phj1698@gmail.com>
2016-05-01 13:33:43 +0900

    Add widget on form

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,28 @@
 from django import forms
 from .models import Habit, Log
+from django.forms import extras
+import datetime
 
 
 class HabitForm(forms.ModelForm):
+    title = forms.CharField(
+        max_length=200,
+        widget=forms.TextInput(attrs={'class': "form-control",
+                                      'placeholder': "Title"}), )
     class Meta:
         model = Habit
         fields = ('title', )
 
 
 class LogForm(forms.ModelForm):
+    date = forms.DateField(
+        initial=datetime.date.today,
+        widget=extras.SelectDateWidget(years=[y for y in range(2010,2020)], ))
+    content = forms.CharField(
+        max_length=500,
+        widget=forms.TextInput(attrs={'class': "form-control",
+                                      'placeholder': "Content"}), )
+    score = forms.IntegerField()
     class Meta:
         model = Log
         fields = ('date', 'content', 'score')
